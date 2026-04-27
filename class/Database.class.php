6140ab45ae973b6744d67d851ea3de0baa1aef86
ProfAlbertoAyres <alberto.benicio@ifro.edu.br>
2026-04-27 15:19:23 -0400

    Criação da classe de conexão

---
+++ class/Database.class.php
@@ -0,0 +1,35 @@
+<?php
+final class Database{
+    private static $instance = null;
+    private $connection;
+
+    private function __construct(){
+       try {
+        $config = parse_ini_file(__DIR__ ."/../config.ini",true)["database"];
+        $dns="";
+        if($config["driver"]=== "mysql"){
+            $dns= "mysql:host={$config['host']};port={$config['port']};dbname={$config['data']};charset=utf8";
+        }elseif($config["driver"]=== "pgsql"){
+            $dns= "pgsql:host={$config['host']};port={$config['port']};dbname={$config['data']}";
+        }else{
+            throw new Exception("Driver de banco de dados não suportado".$config['driver']);
+        }
+        $this->connection = new PDO($dns,$config['user'],$config['pass']);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       } catch (PDOException $e) {
+           error_log('Erro de conexão'.$e->getMessage());
+           die('Erro ao conectar com o banco de dados. Tente mais tarde');
+       }
+    }
+
+    public static function getInstance(){
+        if(self::$instance===null){
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection(){
+        return $this->connection;
+    }
+}
