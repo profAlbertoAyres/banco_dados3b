6c842846dfeebf89f583d1a49b76fccdde929c13
ProfAlbertoAyres <alberto.benicio@ifro.edu.br>
2026-05-06 08:23:50 -0400

    Aula

--- ger-exercicio.php
+++ ger-exercicio.php
@@ -9,10 +9,22 @@
 </head>
 
 <body>
-  <?php require_once "_parts/_menu.php" ?>
+  <?php 
+  require_once "_parts/_menu.php"; 
+  $grupoMuscular = [
+    "Abdômen",
+    "Antebraço",
+    "Bíceps",
+    "Costas",
+    "Glúteo",
+    "Panturrilha",
+    "Peito",
+    "Tríceps"
+  ]
+  ?>
   <main class="container" style="margin-top: 80px;">
     <div class="mt-5">
-        <h4>Cadastro de grupo muscular</h4>
+        <h4>Cadastro de exercício</h4>
     </div>
     <div class="card">
       <form action="" method="post" class="row p-4 g3 mt-3">
@@ -20,6 +32,20 @@
           <label for="nome" class="form-label">Nome</label>
           <input type="text" name="nome" id="nome" class="form-control">
         </div>
+        <div class="col-12">
+          <label for="descricao" class="form-label">Descrição</label>
+          <textarea name="descricao" id="descricao" class="form-control"></textarea>
+        </div>
+        <div class="col-md-6">
+          <label for="grupoMuscular" class="form-label">Grupo Muscular</label>
+          <select name="grupoMuscular" id="grupoMuscular" class="form-select">
+            <option value="">Selecione...</option>
+            <?php foreach( $grupoMuscular as $grupo ) : ?>
+            <option value="<?= $grupo ?>"><?= $grupo ?></option>
+            <?php endforeach; ?>
+          </select>
+        </div>
+
         <div class="mt-3">
           <a href="gmuscular.php" class="btn btn-secondary" >Cancelar</a>
           <button type="submit" class="btn btn-primary">Salvar</button>
